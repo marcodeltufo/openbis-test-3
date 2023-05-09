d7de0be63e552068f8b8e7fb8d6a0072496d8715
marcodeltufo <marco.deltufo@exact-lab.it>
2023-05-09 12:26:16 +0200

    .

---
+++ docs/conf.py
@@ -0,0 +1,7 @@
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_logo = "openBISlogo.png"
+html_theme_options = {
+        'logo_only': False,
+        'display_version': False,
+}
